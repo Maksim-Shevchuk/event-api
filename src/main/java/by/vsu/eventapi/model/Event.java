9473d1316076ad17b8e3ff901f1b0b808f9fa700
Maksim Shevchuk <maskim27012003@gmail.com>
2023-06-13 16:16:23 +0300

    replacement imports with * by single imports

--- src/main/java/by/vsu/eventapi/model/Event.java
+++ src/main/java/by/vsu/eventapi/model/Event.java
@@ -1,9 +1,18 @@
 package by.vsu.eventapi.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import org.hibernate.Hibernate;
 
 import java.sql.Date;
